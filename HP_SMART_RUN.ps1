b7a8dea9fdf1e38373babccc4327fa643ee0752d
atsjason <98031074+atsjason@users.noreply.github.com>
2022-04-19 12:36:07 -0400

    Update HP_SMART_RUN.ps1
    Updated If Condition statement -> VCLib and HPPrinterControl Kept installing due to not updating WindowsApp directory src

--- HP_SMART_RUN.ps1
+++ HP_SMART_RUN.ps1
@@ -18,10 +18,10 @@ Function installFiles(){
     if (!(Test-Path -Path "C:\Program Files\WindowsApps\microsoft.net.native.runtime.2.2_2.2.28604.0_x64__8wekyb3d8bbwe")){
         add-appxpackage -path "$DOWNLOAD_RUNTIME" -ErrorAction Ignore}
 
-    if (!(Test-Path -Path "C:\Program Files\WindowsApps\Microsoft.VCLibs.120.00_12.0.21005.1_x64__8wekyb3d8bbwe")){
+    if (!(Test-Path -Path "C:\Program Files\WindowsApps\Microsoft.VCLibs.140.00_14.0.30704.0_x64__8wekyb3d8bbwe")){
         add-appxpackage -path "$DOWNLOAD_VCLIB" -ErrorAction Ignore}
 
-    if (!(Test-Path -Path "C:\Program Files\WindowsApps\AD2F1837.HPPrinterControl_35.0.97.0_neutral__v10z8vjag6ke6")){
+    if (!(Test-Path -Path "C:\Program Files\WindowsApps\AD2F1837.HPPrinterControl_121.2.195.0_neutral_~_v10z8vjag6ke6")){
         # add-appxpackage -path "$DOWNLOAD_HPCONTROL" -ErrorAction Ignore
         add-appxpackage -path "$DOWNLOAD_HPSMART"}
 
