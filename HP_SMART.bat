fa50e44f38263918b5868d3ae3bdfbea04f0ab2e
atsjason <98031074+atsjason@users.noreply.github.com>
2022-04-19 13:16:58 -0400

    Update HP_SMART.bat
    Edited this version to allow any use of the drive letter %CD%

--- HP_SMART.bat
+++ HP_SMART.bat
@@ -1,2 +1,4 @@
-"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -ExecutionPolicy Bypass -File "D:\HP_SMART_RUN.ps1"
+"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -ExecutionPolicy Bypass -File "%CD%\HP_SMART_RUN.ps1"
+TIMEOUT /T 4
+"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -ExecutionPolicy Bypass -File "%CD%\HP_SMART_DESKTOP_SHORTCUT.ps1"
 pause
