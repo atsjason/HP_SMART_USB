5fca194da343d645b9f1c4f1ea0bcc3257fbab17
atsjason <98031074+atsjason@users.noreply.github.com>
2022-05-05 15:10:49 -0400

    Update HP_SMART_RUN.ps1

--- HP_SMART_RUN.ps1
+++ HP_SMART_RUN.ps1
@@ -1,13 +1,14 @@
 
 $CURRENT_PATH = Get-Location
 
-$DOWNLOAD_RUNTIME =    Join-Path -Path $CURRENT_PATH.Path -ChildPath "Microsoft.NET.Native.Runtime.2.2_2.2.28604.0_x64__8wekyb3d8bbwe.Appx"
-$DOWNLOAD_FRAMEWORK =  Join-Path -Path $CURRENT_PATH.Path -ChildPath "Microsoft.NET.Native.Framework.2.2_2.2.29512.0_x64__8wekyb3d8bbwe.Appx"
-$DOWNLOAD_VCLIB =      Join-Path -Path $CURRENT_PATH.Path -ChildPath "Microsoft.VCLibs.140.00_14.0.30704.0_x64__8wekyb3d8bbwe.Appx"
+$DOWNLOAD_RUNTIME =    Join-Path -Path $CURRENT_PATH.Path -ChildPath "HP_Smart_Resource\Microsoft.NET.Native.Runtime.2.2_2.2.28604.0_x64__8wekyb3d8bbwe.Appx"
+$DOWNLOAD_FRAMEWORK =  Join-Path -Path $CURRENT_PATH.Path -ChildPath "HP_Smart_Resource\Microsoft.NET.Native.Framework.2.2_2.2.29512.0_x64__8wekyb3d8bbwe.Appx"
+$DOWNLOAD_VCLIB =      Join-Path -Path $CURRENT_PATH.Path -ChildPath "HP_Smart_Resource\Microsoft.VCLibs.140.00_14.0.30704.0_x64__8wekyb3d8bbwe.Appx"
 # $DOWNLOAD_HPCONTROL =  "C:\Windows\Temp\HP Smart\AD2F1837.HPPrinterControl_35.0.97.0_neutral__v10z8vjag6ke6.Appx"
-$DOWNLOAD_HPSMART =    Join-Path -Path $CURRENT_PATH.Path -ChildPath "AD2F1837.HPPrinterControl_121.2.195.0_neutral___v10z8vjag6ke6.AppxBundle"
+$DOWNLOAD_HPSMART =    Join-Path -Path $CURRENT_PATH.Path -ChildPath "HP_Smart_Resource\AD2F1837.HPPrinterControl_121.2.195.0_neutral___v10z8vjag6ke6.AppxBundle"
 
-
+# Links are from https://store.rg-adguard.net/
+# HP Smart URL: https://www.microsoft.com/store/productId/9WZDNCRFHWLH
 
 
 Function installFiles(){
