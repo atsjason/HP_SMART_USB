7f264dcbbc3a510bcf1b26e18f0c9344783fd194
atsjason <98031074+atsjason@users.noreply.github.com>
2022-05-05 15:08:43 -0400

    Update HP_SMART.bat

--- HP_SMART.bat
+++ HP_SMART.bat
@@ -1,4 +1,4 @@
-"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -ExecutionPolicy Bypass -File "%CD%\HP_SMART_RUN.ps1"
+"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -ExecutionPolicy Bypass -File "%CD%\HP_Smart_Resource\HP_SMART_RUN.ps1"
 TIMEOUT /T 4
-"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -ExecutionPolicy Bypass -File "%CD%\HP_SMART_DESKTOP_SHORTCUT.ps1"
+"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -ExecutionPolicy Bypass -File "%CD%\HP_Smart_Resource\HP_SMART_DESKTOP_SHORTCUT.ps1"
 pause
